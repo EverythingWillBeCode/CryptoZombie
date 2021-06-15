d689df758f888737dc7ef82d411e3dc67317a882
cchiramberro <67934837+EverythingWillBeCode@users.noreply.github.com>
2021-06-15 20:49:33 -0300

    Ejercicio 7. Completado

---
+++ Lesson2/zombieFeeding.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.19;
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    // empieza aquí
+  }
+
+}
